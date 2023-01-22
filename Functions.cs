b178798dfbe3371019134efbe797432f1c42f65d
517201965 <517201965@qq.com>
2023-01-22 22:56:32 +0800

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDMJClient_Single
+{
+    class Functions
+    {
+    }
+    public static class ZUOWEI
+    {
+        static public int Kong = 4;
+        static public int Dong = 0;
+        static public int Nan = 1;
+        static public int Xi = 2;
+        static public int Bei = 3;
+        static public string[] FengXiang = new string[5] { "东", "南", "西", "北", "空" };
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.Pipes;
+
+namespace BDMJClient_Single
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dong.Zuowei.Text = "东";
+            //using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut))
+            //{
+            //    pipeServer.WaitForConnection();
+
+            //    var data = new byte[10240];
+            //    var count = pipeServer.Read(data, 0, 10240);
+
+            //    using (StreamWriter sw = new StreamWriter(pipeServer))
+            //    {
+            //        sw.AutoFlush = true;
+            //        sw.WriteLine("send to client");
+            //    }
+            //}
+        }
+    }
+}
